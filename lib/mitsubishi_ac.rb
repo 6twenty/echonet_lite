6f41b0667002b12fead936c3e337874f9d6a1f5f
Mike Fulcher <michael.fulcher@gmail.com>
2019-03-01 18:57:16 +1300

    Initial commit

---
+++ lib/mitsubishi_ac.rb
@@ -0,0 +1,6 @@
+require "mitsubishi_ac/version"
+
+module MitsubishiAc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mitsubishi_ac/version.rb
@@ -0,0 +1,3 @@
+module MitsubishiAc
+  VERSION = "0.1.0"
+end
